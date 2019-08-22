508899bbf70d628e5bb4127d2da0740cebd46f74
malarc01 <45378877+malarc01@users.noreply.github.com>
2019-08-21 20:24:26 -0400

    added options headers

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 app.listen(6780, () => {
-	console.log('listening @ 3K');
+	console.log('listening @ 6780');
 });
 
 app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+app.post('/api', (request, response) => {
+	console.log('REQUEST MADE');
+	console.log(request.body);
+	response.json({
+		status: 'success',
+		latitude: data.lat,
+		longitude: data.long
+	});
+});
